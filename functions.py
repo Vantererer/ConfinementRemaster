ada42a2c13ab6218283e047337b629946b7f1dce
Vantererer <89364887+Vantererer@users.noreply.github.com>
2024-03-07 08:40:34 +1300

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,4 +1,17 @@
-def match_from_array(data: str, array) -> bool:
+import sys
+import time
+
+Deflay = 35 
+
+def sl(ms: int):
+    time.sleep(ms / 1000)
+
+wait, delay = sl, sl # feel free to change the code (6-9)
+
+def clean():
+    print("/033c", end='')
+
+def match_from_array(data: str, array) -> bool: #forgor what trhis was
     out = False
     for x in array:
         if x == data:
@@ -8,3 +21,12 @@ def match_from_array(data: str, array) -> bool:
         else:
             return False
 
+def twriter (text: str, delaychar: int): # Type writer effect
+    if not text:
+        print("Missing Text Error")
+    if not delaychar:
+        delaychar = Deflay
+    for char in text:
+        sys.std.write(char)
+        sl(delaychar)
+        sys.stdout.flush()
