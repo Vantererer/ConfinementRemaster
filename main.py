a5fd4c8f984704d27b77181c4d78224b6f0be861
Vantererer <89364887+Vantererer@users.noreply.github.com>
2024-03-11 13:28:41 +1300

    Update main.py
    Working more on the vent option and made sure you can get the wrench, I need to optimize the code a bit more.

--- main.py
+++ main.py
@@ -21,7 +21,6 @@ def creator():
 def intro():
     print(
         character_name + ' wakes up in a confined, medium sized room. All that goes through their head is just how did they get there?')
-    sleep(3)
     x = better_input(
         "That doesn't matter, your goal is to help them get out of this weird area they are not famillar this, do you wish to cooperate? [y] [n] ",
         ["y", "y", "n", "n"])
@@ -32,41 +31,62 @@ def intro():
         print("So you leave ", +character_name + " to die, interesting.")
 
 
-def main(wrenchevent=True):
+def main(draw = {'isEmpty': False}):
+
     print(
         character_name + " stands in the center of the room, all thats around them is a vent, a bed, a desk with a laptop on it and a steel door with a keypad on it, what does " + character_name + " do?")
-    x = better_input("Check Vent [1] Check Desk [2] Check Door [3] Check Bed [4] ", ["1", "2", "3", "4"])
+    x = better_input("Check Vent [1] Check Desk [2] Check Door [3] Check Bed [4] Check Items [5] ", ["1", "2", "3", "4", "5"])
     if match_from_array(x, ['1', '1']):  # "Check Vent"
-        print("Vent")
+        print("They walk to the vent and look inside, theres a orange crowbar inside of there, perhaps they could open the vent with a tool.")
+        wait(2)
+    if 'Wrench' in items:
+        print("t")
+        main()
+
     elif match_from_array(x, ['2', '2']):  # "Check Desk"
         x = better_input(
             character_name + " is at the desk, theres a laptop with power on and a few drawers, what do they do? [1] Check Laptop [2] Check the drawers [3] Go back ",
             ["1", "2", "3"])
         if match_from_array(x, ['1', '1']):
-            print("test")
+            print("t")
         elif match_from_array(x, ['2', '2']):
             x = better_input(
-                "They open the drawer and find a wrench, the wrench looks rusty and seems like it can be used for something, does " + character_name + " want to take the wrench? [y] [n] ",
+                "There is one drawer that can be opened, the rest don't budge. Does " + character_name + " want to open it? [y] [n] ",
                 ['y', 'y', 'n', 'n'])
             if match_from_array(x, ['y', 'y']):
-                print("hi")
-                wrenchevent = False
-                print(wrenchevent)
-                main()
-            if not wrenchevent:
-                print("bye")
-                main()
+                if not draw["isEmpty"]:
+                    print(character_name+" finds a wrench and they take it, perhaps they'll use it for something.")
+                    items.append("Wrench")
+                    draw["isEmpty"] = True
+                    main()
+                else:
+                    print("The drawers are empty now, no need to interact with them or look more.")
+                if not draw["isEmpty"]:
+                    print(character_name+" takes the wrench, perhaps they'll use it for something.")
+                    draw["isEmpty"] = True
+                    main()
+                else:
+                    main()
+
+                if not draw["isEmpty"]:
+                    print(character_name+" finds a wrench and they take it, perhaps they'll use it for something.")
+                    draw["isEmpty"] = True
+                    main()
+                else:
+                    main()
 
 
 
 
 
-        elif match_from_array(x, ['3', '3']):
-            main()
+
     elif match_from_array(x, ['3', '3']):  # "Check Door"
         print("Door")
     elif match_from_array(x, ['4', '4']):  # "Check Bed"
         print("Bed")
+    elif match_from_array(x, ['5','5']):   # 'Check Items'
+        print(items)
+        main()
 
 
 def test():
